5e1a74f1b0898928b78ceaf51fec003b0469e3ae
toufeeqgoudia <toufeeqgoudia@gmail.com>
2023-10-30 15:12:15 +0200

    initial reactjs setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Full Stack Todo App
+
+## Backend:
+- Python and Django
+
+## Frontend:
+- JavaScript and ReactJS
---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1>Todo App</h1>
+    </>
+  );
+};
+
+export default App;
